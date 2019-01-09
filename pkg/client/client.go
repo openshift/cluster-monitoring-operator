ac5dfb8c8a1bc8d949d9c2fc6c49bd9894825b1c
Frederic Branczyk <fbranczyk@gmail.com>
2019-01-09 14:24:20 +0100

    Set ResourceVersion when updating Prometheus Operator objects

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -223,7 +223,7 @@ func (c *Client) NamespacesToMonitor() ([]string, error) {
 
 func (c *Client) CreateOrUpdatePrometheus(p *monv1.Prometheus) error {
 	pclient := c.mclient.MonitoringV1().Prometheuses(p.GetNamespace())
-	_, err := pclient.Get(p.GetName(), metav1.GetOptions{})
+	oldProm, err := pclient.Get(p.GetName(), metav1.GetOptions{})
 	if apierrors.IsNotFound(err) {
 		_, err := pclient.Create(p)
 		return errors.Wrap(err, "creating Prometheus object failed")
@@ -232,13 +232,14 @@ func (c *Client) CreateOrUpdatePrometheus(p *monv1.Prometheus) error {
 		return errors.Wrap(err, "retrieving Prometheus object failed")
 	}
 
+	p.ResourceVersion = oldProm.ResourceVersion
 	_, err = pclient.Update(p)
 	return errors.Wrap(err, "updating Prometheus object failed")
 }
 
 func (c *Client) CreateOrUpdatePrometheusRule(p *monv1.PrometheusRule) error {
 	pclient := c.mclient.MonitoringV1().PrometheusRules(p.GetNamespace())
-	_, err := pclient.Get(p.GetName(), metav1.GetOptions{})
+	oldRule, err := pclient.Get(p.GetName(), metav1.GetOptions{})
 	if apierrors.IsNotFound(err) {
 		_, err := pclient.Create(p)
 		return errors.Wrap(err, "creating PrometheusRule object failed")
@@ -247,13 +248,14 @@ func (c *Client) CreateOrUpdatePrometheusRule(p *monv1.PrometheusRule) error {
 		return errors.Wrap(err, "retrieving PrometheusRule object failed")
 	}
 
+	p.ResourceVersion = oldRule.ResourceVersion
 	_, err = pclient.Update(p)
 	return errors.Wrap(err, "updating PrometheusRule object failed")
 }
 
 func (c *Client) CreateOrUpdateAlertmanager(a *monv1.Alertmanager) error {
 	aclient := c.mclient.MonitoringV1().Alertmanagers(a.GetNamespace())
-	_, err := aclient.Get(a.GetName(), metav1.GetOptions{})
+	oldAm, err := aclient.Get(a.GetName(), metav1.GetOptions{})
 	if apierrors.IsNotFound(err) {
 		_, err := aclient.Create(a)
 		return errors.Wrap(err, "creating Alertmanager object failed")
@@ -262,6 +264,7 @@ func (c *Client) CreateOrUpdateAlertmanager(a *monv1.Alertmanager) error {
 		return errors.Wrap(err, "retrieving Alertmanager object failed")
 	}
 
+	a.ResourceVersion = oldAm.ResourceVersion
 	_, err = aclient.Update(a)
 	return errors.Wrap(err, "updating Alertmanager object failed")
 }
@@ -719,7 +722,7 @@ func (c *Client) CreateOrUpdateServiceAccount(sa *v1.ServiceAccount) error {
 
 func (c *Client) CreateOrUpdateServiceMonitor(sm *monv1.ServiceMonitor) error {
 	smClient := c.mclient.MonitoringV1().ServiceMonitors(sm.GetNamespace())
-	_, err := smClient.Get(sm.GetName(), metav1.GetOptions{})
+	oldSm, err := smClient.Get(sm.GetName(), metav1.GetOptions{})
 	if apierrors.IsNotFound(err) {
 		_, err := smClient.Create(sm)
 		return errors.Wrap(err, "creating ServiceMonitor object failed")
@@ -728,6 +731,7 @@ func (c *Client) CreateOrUpdateServiceMonitor(sm *monv1.ServiceMonitor) error {
 		return errors.Wrap(err, "retrieving ServiceMonitor object failed")
 	}
 
+	sm.ResourceVersion = oldSm.ResourceVersion
 	_, err = smClient.Update(sm)
 	return errors.Wrap(err, "updating ServiceMonitor object failed")
 }
