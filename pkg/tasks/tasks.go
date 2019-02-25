4da55c9a210b8b6b06fb6aab98d6ad57a4adaaf8
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2019-02-25 14:31:20 +0100

    pkg/tasks: execute tasks concurrently
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1678475

--- pkg/tasks/tasks.go
+++ pkg/tasks/tasks.go
@@ -18,6 +18,7 @@ import (
 	"github.com/golang/glog"
 	"github.com/openshift/cluster-monitoring-operator/pkg/client"
 	"github.com/pkg/errors"
+	"golang.org/x/sync/errgroup"
 )
 
 type TaskRunner struct {
@@ -33,16 +34,22 @@ func NewTaskRunner(client *client.Client, tasks []*TaskSpec) *TaskRunner {
 }
 
 func (tl *TaskRunner) RunAll() error {
+	var g errgroup.Group
+
 	for i, ts := range tl.tasks {
-		glog.V(4).Infof("running task %d of %d: %v", i+1, len(tl.tasks), ts.Name)
-		err := tl.ExecuteTask(ts)
-		if err != nil {
+		// shadow vars due to concurrency
+		ts := ts
+		i := i
+
+		g.Go(func() error {
+			glog.V(4).Infof("running task %d of %d: %v", i+1, len(tl.tasks), ts.Name)
+			err := tl.ExecuteTask(ts)
+			glog.V(4).Infof("ran task %d of %d: %v", i+1, len(tl.tasks), ts.Name)
 			return errors.Wrapf(err, "running task %v failed", ts.Name)
-		}
-		glog.V(4).Infof("ran task %d of %d: %v", i+1, len(tl.tasks), ts.Name)
+		})
 	}
 
-	return nil
+	return g.Wait()
 }
 
 func (tl *TaskRunner) ExecuteTask(ts *TaskSpec) error {
