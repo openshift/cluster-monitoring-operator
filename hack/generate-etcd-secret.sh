5f28e3d9c0194613013fb06823c777a9e80e2e52
Frederic Branczyk <fbranczyk@gmail.com>
2019-02-28 17:53:53 +0100

    hack: Adapt generate etcd secret script to 4.0

--- hack/generate-etcd-secret.sh
+++ hack/generate-etcd-secret.sh
@@ -4,17 +4,18 @@ set -x
 # only exit with zero if all commands of the pipeline exit successfully
 set -o pipefail
 
-IP=${1}
+NAMESPACE=openshift-kube-apiserver
+APISERVERPODNAME="$(kubectl -n ${NAMESPACE} get pod -lapp=openshift-kube-apiserver -ojsonpath='{.items[0].metadata.name}')"
 
 cat <<-EOF
 apiVersion: v1
-data:
-  etcd-client-ca.crt: "$(ssh -i ~/go/src/github.com/openshift/release/cluster/test-deploy/gcp-dev/ssh-privatekey cloud-user@$IP sudo -E cat /etc/origin/master/master.etcd-ca.crt | base64 --wrap=0)"
-  etcd-client.crt: "$(ssh -i ~/go/src/github.com/openshift/release/cluster/test-deploy/gcp-dev/ssh-privatekey cloud-user@$IP sudo -E cat /etc/origin/master/master.etcd-client.crt | base64 --wrap=0)"
-  etcd-client.key: "$(ssh -i ~/go/src/github.com/openshift/release/cluster/test-deploy/gcp-dev/ssh-privatekey cloud-user@$IP sudo -E cat /etc/origin/master/master.etcd-client.key | base64 --wrap=0)"
 kind: Secret
 metadata:
   name: kube-etcd-client-certs
   namespace: openshift-monitoring
 type: Opaque
+data:
+  etcd-client-ca.crt: "$(oc rsh -n ${NAMESPACE} ${APISERVERPODNAME} cat /etc/kubernetes/static-pod-resources/configmaps/etcd-serving-ca/ca-bundle.crt | base64 --wrap=0)"
+  etcd-client.crt: "$(oc rsh -n ${NAMESPACE} ${APISERVERPODNAME} cat /etc/kubernetes/static-pod-resources/secrets/etcd-client/tls.crt | base64 --wrap=0)"
+  etcd-client.key: "$(oc rsh -n ${NAMESPACE} ${APISERVERPODNAME} cat /etc/kubernetes/static-pod-resources/secrets/etcd-client/tls.key | base64 --wrap=0)"
 EOF
