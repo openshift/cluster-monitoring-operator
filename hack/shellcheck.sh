0223e397ce976d740c53bdae33f48bc130180a17
paulfantom <pawel@krupa.net.pl>
2019-07-19 10:33:04 +0200

    hack: add script to run shellcheck in a container and outside of it

---
+++ hack/shellcheck.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -eux
+
+if [ "$IS_CONTAINER" != "" ]; then
+  TOP_DIR="${1:-.}"
+  find "${TOP_DIR}" \
+    -path "${TOP_DIR}/vendor" -prune \
+    -o -path "${TOP_DIR}/jsonnet/vendor" -prune \
+    -o -type f -name '*.sh' -exec shellcheck --format=gcc {} \+
+else
+  docker run --rm \
+    --env IS_CONTAINER=TRUE \
+    --volume "${PWD}:/workdir:ro,z" \
+    --entrypoint sh \
+    --workdir /workdir \
+    koalaman/shellcheck-alpine:stable \
+    /workdir/hack/shellcheck.sh "${@}"
+fi;
