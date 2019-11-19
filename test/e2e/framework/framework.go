539414ae86edc00e1be75bc7b4e097edc65f0554
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2019-11-19 14:59:35 +0100

    test/e2e/framework: add AlertmanagerClient

--- test/e2e/framework/framework.go
+++ test/e2e/framework/framework.go
@@ -49,6 +49,7 @@ type Framework struct {
 	KubeClient          kubernetes.Interface
 	ThanosQuerierClient *RouteClient
 	PrometheusK8sClient *RouteClient
+	AlertmanagerClient  *RouteClient
 	APIServicesClient   *apiservicesclient.Clientset
 	MetricsClient       *metricsclient.Clientset
 
@@ -134,6 +135,14 @@ func New(kubeConfigPath string) (*Framework, cleanUpFunc, error) {
 		return nil, nil, errors.Wrap(err, "creating PrometheusK8sClient failed")
 	}
 
+	f.AlertmanagerClient, err = NewRouteClient(
+		openshiftRouteClient, kubeClient,
+		"openshift-monitoring", "alertmanager-main",
+	)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "creating AlertmanagerClient failed")
+	}
+
 	return f, cleanUp, nil
 }
 
@@ -151,6 +160,13 @@ func (f *Framework) setup() (cleanUpFunc, error) {
 	cleanUpFuncs = append(cleanUpFuncs, cf)
 
 	cf, err = f.CreateClusterRoleBinding()
+	if err != nil {
+		return nil, err
+	}
+
+	cleanUpFuncs = append(cleanUpFuncs, cf)
+
+	cf, err = f.CreateAlertmanagerRoleBinding()
 	if err != nil {
 		return nil, err
 	}
@@ -222,6 +238,35 @@ func (f *Framework) CreateClusterRoleBinding() (cleanUpFunc, error) {
 
 	return func() error {
 		return f.KubeClient.RbacV1().ClusterRoleBindings().Delete(clusterRoleBinding.Name, &metav1.DeleteOptions{})
+	}, nil
+}
+
+func (f *Framework) CreateAlertmanagerRoleBinding() (cleanUpFunc, error) {
+	roleBinding := &rbacv1.RoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "cluster-monitoring-operator-e2e",
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      "cluster-monitoring-operator-e2e",
+				Namespace: "openshift-monitoring",
+			},
+		},
+		RoleRef: rbacv1.RoleRef{
+			Kind:     "Role",
+			Name:     "alertmanager-access",
+			APIGroup: "rbac.authorization.k8s.io",
+		},
+	}
+
+	roleBinding, err := f.KubeClient.RbacV1().RoleBindings(namespaceName).Create(roleBinding)
+	if err != nil {
+		return nil, err
+	}
+
+	return func() error {
+		return f.KubeClient.RbacV1().RoleBindings(namespaceName).Delete(roleBinding.Name, &metav1.DeleteOptions{})
 	}, nil
 }
 
