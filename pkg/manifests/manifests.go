411fec64bdf51486233f0da563be9a88d1a59ad0
Philip Gough <philip.p.gough@gmail.com>
2021-08-17 13:50:48 +0100

    manifest: Adds PrometheusRule for Thanos Sidecar
    As per https://github.com/prometheus-operator/kube-prometheus/pull/1308,
    a new PrometheusRule will be generated for Thanos Sidecar. This change includes
    that generated file.

--- pkg/manifests/manifests.go
+++ pkg/manifests/manifests.go
@@ -98,6 +98,7 @@ var (
 	PrometheusK8sRoleConfig                  = "prometheus-k8s/role-config.yaml"
 	PrometheusK8sRoleList                    = "prometheus-k8s/role-specific-namespaces.yaml"
 	PrometheusK8sPrometheusRule              = "prometheus-k8s/prometheus-rule.yaml"
+	PrometheusK8sThanosSidecarPrometheusRule = "prometheus-k8s/prometheus-rule-thanos-sidecar.yaml"
 	PrometheusK8sServiceAccount              = "prometheus-k8s/service-account.yaml"
 	PrometheusK8s                            = "prometheus-k8s/prometheus.yaml"
 	PrometheusK8sPrometheusServiceMonitor    = "prometheus-k8s/service-monitor.yaml"
