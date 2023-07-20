491b2c1290d1d3f87f8a7af393f755b5a0cf8967
Joao Marcal <jmarcal@redhat.com>
2023-07-20 17:13:52 +0100

    Refactored and implemented ProfileGenerator

--- hack/monitorgen/internal/profiles/minimal.go
+++ hack/monitorgen/internal/profiles/minimal.go
@@ -1,8 +1,6 @@
 package profiles
 
 import (
-	"fmt"
-
 	metricsprofiles "github.com/openshift/cluster-monitoring-operator/hack/monitorgen/internal/metrics"
 	"github.com/openshift/cluster-monitoring-operator/pkg/manifests"
 
@@ -21,49 +19,41 @@ var (
 	}
 )
 
+type MinimalProfileGenerator struct {
+	parentSMPath, minimalMetricsFilePath string
+}
+
+func (mpg MinimalProfileGenerator) PatchServiceMonitor(sm *monitoringv1.ServiceMonitor, metrics []string) {
+	for i := 0; i < len(sm.Spec.Endpoints); i++ {
+		endpoint := &sm.Spec.Endpoints[i]
+		// Remove drop relabel configs
+		if len(endpoint.MetricRelabelConfigs) > 0 {
+			endpoint.MetricRelabelConfigs = removeDrop(endpoint.MetricRelabelConfigs)
+		}
+		// Add keep relabel configs
+		endpoint.MetricRelabelConfigs = append(endpoint.MetricRelabelConfigs, keepMetrics(metrics))
+	}
+}
+
+func (mpg MinimalProfileGenerator) Profile() manifests.CollectionProfile {
+	return manifests.MinimalCollectionProfile
+}
+
+func (mpg MinimalProfileGenerator) DefaultMetrics() map[string][]string {
+	return minimalProfileMetrics
+}
+
+func (mpg MinimalProfileGenerator) ParentSMPath() string {
+	return mpg.parentSMPath
+}
+
+func (mpg MinimalProfileGenerator) MetricsFilePath() string {
+	return mpg.minimalMetricsFilePath
+}
+
 func GenerateCollectionProfileMinimal(sm monitoringv1.ServiceMonitor, parentSMPath, minimalMetricsFilePath string) {
-	generateProfile(sm, parentSMPath, minimalMetricsFilePath, manifests.MinimalCollectionProfile, minimalProfileMetrics, func(sm *monitoringv1.ServiceMonitor, metrics []string) {
-		for i := 0; i < len(sm.Spec.Endpoints); i++ {
-			endpoint := &sm.Spec.Endpoints[i]
-			// Remove drop relabel configs
-			if len(endpoint.MetricRelabelConfigs) > 0 {
-				endpoint.MetricRelabelConfigs = removeDrop(endpoint.MetricRelabelConfigs)
-			}
-			// Add keep relabel configs
-			endpoint.MetricRelabelConfigs = append(endpoint.MetricRelabelConfigs, keepMetrics(metrics))
-		}
-	})
-}
-
-// removeDrop goes through metricRelabelConfigs and
-// returns a copy of metricRelabelConfigs without relabelConfigs that have the
-// action "drop"
-func removeDrop(metricRelabelConfigs []*monitoringv1.RelabelConfig) []*monitoringv1.RelabelConfig {
-	mrc := make([]*monitoringv1.RelabelConfig, 1)
-	for _, relabelConfig := range metricRelabelConfigs {
-		if relabelConfig.Action == "drop" {
-			continue
-		}
-		mrc = append(mrc, relabelConfig)
-	}
-
-	return mrc
-}
-
-// keepMetrics goes through the metrics in the slice metrics and joins
-// in a string with "|", them returns a relabelConfig with action "keep" and the
-// joined metrics in the regex field.
-func keepMetrics(metrics []string) *monitoringv1.RelabelConfig {
-	jointMetrics := metrics[0]
-	for i := 1; i < len(metrics); i++ {
-		jointMetrics = jointMetrics + "|" + metrics[i]
-	}
-
-	return &monitoringv1.RelabelConfig{
-		Action: "keep",
-		SourceLabels: []monitoringv1.LabelName{
-			"__name__",
-		},
-		Regex: fmt.Sprintf("(%s)", jointMetrics),
-	}
+	generateProfile(&MinimalProfileGenerator{
+		parentSMPath:           parentSMPath,
+		minimalMetricsFilePath: minimalMetricsFilePath,
+	}, sm)
 }
--- hack/monitorgen/internal/profiles/var.go
+++ hack/monitorgen/internal/profiles/var.go
@@ -8,36 +8,44 @@ import (
 
 	"github.com/openshift/cluster-monitoring-operator/pkg/manifests"
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
-	"gopkg.in/yaml.v2"
+	"sigs.k8s.io/yaml"
 )
 
-func generateProfile(sm monitoringv1.ServiceMonitor, parentSMPath, metricsFilePath, profile string, defaultMetrics map[string][]string, patchSM func(sm *monitoringv1.ServiceMonitor, metrics []string)) {
+type ProfilesGenerator interface {
+	Profile() manifests.CollectionProfile
+	PatchServiceMonitor(sm *monitoringv1.ServiceMonitor, metrics []string)
+	DefaultMetrics() map[string][]string
+	ParentSMPath() string
+	MetricsFilePath() string
+}
+
+func generateProfile(pg ProfilesGenerator, sm monitoringv1.ServiceMonitor) {
 	var err error
 
 	// Minimal set of metrics to keep based on the hard coded value in
 	// minimalProfileMetrics
-	metricsToKeep := defaultMetrics[sm.Name]
+	metrics := pg.DefaultMetrics()[sm.Name]
 
 	// Overwrite previous value if the user specified a file path
-	if metricsFilePath != "" {
-		metricsToKeep, err = readFileByLine(metricsFilePath)
+	if pg.MetricsFilePath() != "" {
+		metrics, err = readFileByLine(pg.MetricsFilePath())
 		if err != nil {
 			log.Fatalf("failed to update metrics to keep from file: %e", err)
 		}
 	}
 
-	if len(metricsToKeep) == 0 {
+	if len(metrics) == 0 {
 		return //nothing to do
 	}
 
 	newSM := sm.DeepCopy()
-	newSM.Name = fmt.Sprintf("%s-%s", newSM.Name, profile)
-	newSM.Labels[manifests.CollectionProfileLabel] = profile
-	patchSM(newSM, metricsToKeep)
-	writeServiceMonitorToFile(parentSMPath, *newSM)
+	newSM.Name = fmt.Sprintf("%s-%s", newSM.Name, pg.Profile())
+	newSM.Labels[manifests.CollectionProfileLabel] = string(pg.Profile())
+	pg.PatchServiceMonitor(newSM, metrics)
+	writeServiceMonitorToFile(pg.ParentSMPath(), newSM)
 }
 
-func writeServiceMonitorToFile(fullSMPath string, sm monitoringv1.ServiceMonitor) {
+func writeServiceMonitorToFile(fullSMPath string, sm *monitoringv1.ServiceMonitor) {
 	// Take a file name like metrics.yaml and add "-minimal" between
 	// "." and "yaml" resulting in "metrics-minimal.yaml"
 	profile := sm.Labels[manifests.CollectionProfileLabel]
@@ -61,4 +69,37 @@ func readFileByLine(path string) ([]string, error) {
 		return nil, fmt.Errorf("failed to read metrics file: %e", err)
 	}
 	return strings.Split(string(f), "\n"), nil
+}
+
+// removeDrop goes through metricRelabelConfigs and
+// returns a copy of metricRelabelConfigs without relabelConfigs that have the
+// action "drop"
+func removeDrop(metricRelabelConfigs []*monitoringv1.RelabelConfig) []*monitoringv1.RelabelConfig {
+	mrc := make([]*monitoringv1.RelabelConfig, 1)
+	for _, relabelConfig := range metricRelabelConfigs {
+		if relabelConfig.Action == "drop" {
+			continue
+		}
+		mrc = append(mrc, relabelConfig)
+	}
+
+	return mrc
+}
+
+// keepMetrics goes through the metrics in the slice metrics and joins
+// in a string with "|", them returns a relabelConfig with action "keep" and the
+// joined metrics in the regex field.
+func keepMetrics(metrics []string) *monitoringv1.RelabelConfig {
+	jointMetrics := metrics[0]
+	for i := 1; i < len(metrics); i++ {
+		jointMetrics = jointMetrics + "|" + metrics[i]
+	}
+
+	return &monitoringv1.RelabelConfig{
+		Action: "keep",
+		SourceLabels: []monitoringv1.LabelName{
+			"__name__",
+		},
+		Regex: fmt.Sprintf("(%s)", jointMetrics),
+	}
 }
