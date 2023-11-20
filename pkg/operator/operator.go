7d6d2600416efbe3e65c31e17fe76bf8b32b30e9
Ayoub Mrini <amrini@redhat.com>
2023-11-20 18:20:02 +0100

    revert

--- pkg/operator/operator.go
+++ pkg/operator/operator.go
@@ -691,10 +691,10 @@ func (o *Operator) sync(ctx context.Context, key string) error {
 			[]*tasks.TaskSpec{
 				tasks.NewTaskSpec("Updating metrics scraping client CA", tasks.NewMetricsClientCATask(o.client, factory, config)),
 				tasks.NewTaskSpec("Updating Prometheus Operator", tasks.NewPrometheusOperatorTask(o.client, factory)),
-				tasks.NewTaskSpec("Updating user workload Prometheus Operator", tasks.NewPrometheusOperatorUserWorkloadTask(o.client, factory, config)),
 			}),
 		tasks.NewTaskGroup(
 			[]*tasks.TaskSpec{
+				tasks.NewTaskSpec("Updating user workload Prometheus Operator", tasks.NewPrometheusOperatorUserWorkloadTask(o.client, factory, config)),
 				tasks.NewTaskSpec("Updating Cluster Monitoring Operator", tasks.NewClusterMonitoringOperatorTask(o.client, factory, config)),
 				tasks.NewTaskSpec("Updating Prometheus-k8s", tasks.NewPrometheusTask(o.client, factory, config)),
 				tasks.NewTaskSpec("Updating Prometheus-user-workload", tasks.NewPrometheusUserWorkloadTask(o.client, factory, config)),
