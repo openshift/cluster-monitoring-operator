64213e7443ca0e21d21c4f8d001d87675b91f7f6
Ayoub Mrini <amrini@redhat.com>
2023-12-29 12:08:31 +0100

    chore(versions): move hack/generate-versions.sh into hack/tools/generate_versions.go and provide components versions for multiple OCP versions

---
+++ hack/go/generate_versions.go
@@ -0,0 +1,218 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"slices"
+	"sort"
+	"strings"
+
+	"github.com/blang/semver/v4"
+	"github.com/hashicorp/go-retryablehttp"
+	"github.com/olekukonko/tablewriter"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	mainBranch        = "master"
+	metricsServerRepo = "openshift/kubernetes-metrics-server"
+	versionFile       = "../../jsonnet/versions.yaml"
+	versionNotFound     = "N/A"
+	OCPVersionHeader   = " OCP Version"
+	depsVersionsFile    = "../../Documentation/deps-versions.md"
+	versionFileComments = `---
+# This file is meant to be managed by hack/go/generate_versions.go script
+# Versions provided here are mapped to 'app.kubernetes.io/version' label in all generated manifests
+
+`
+)
+
+type Components struct {
+	Repos    map[string]string `yaml:"repos"`
+	Versions map[string]string `yaml:"versions"`
+}
+
+func main() {
+	// PULL_BASE_REF will be set by CI.
+	pullBaseRef, set := os.LookupEnv("PULL_BASE_REF")
+	if !set {
+		pullBaseRef = mainBranch
+	}
+
+	if pullBaseRef != mainBranch {
+		log.Printf(
+			"Components versions are only updated on '%s' branch for now. Nothing to do against the branch '%s'.",
+			mainBranch,
+			pullBaseRef,
+		)
+		os.Exit(0)
+	}
+
+	data, err := os.ReadFile(versionFile)
+	if err != nil {
+		log.Fatalf("error reading file: %v", err)
+	}
+	components := Components{}
+	err = yaml.Unmarshal(data, &components)
+	if err != nil {
+		log.Fatalf("error while unmarshalling: %v", err)
+	}
+
+	err = updateVersionFile(versionFile, components)
+	if err != nil {
+		log.Fatalf("error updating %s: %v", versionFile, err)
+	}
+	err = updateDepsVersionsFile(depsVersionsFile, components)
+	if err != nil {
+		log.Fatalf("error updating %s: %v", versionFile, err)
+	}
+}
+
+func trimmedVersion(rawVersion string) string {
+	return strings.TrimSuffix(strings.TrimPrefix(rawVersion, "v"), "\n")
+}
+
+func updateVersionFile(fileP string, components Components) error {
+	keys := []string{}
+	for component := range components.Repos {
+		keys = append(keys, component)
+	}
+	sort.Strings(keys)
+
+	for _, component := range keys {
+		knownVersion, _ := components.Versions[component]
+		newVersion, err := getVersion(components.Repos[component], mainBranch)
+		if err != nil {
+			log.Fatalf("couldn't fetch the new version for %s: %v", component, err)
+		}
+		newVersion = trimmedVersion(newVersion)
+		if newVersion != knownVersion {
+			log.Printf("%s version changed from '%s' to '%s'", component, knownVersion, newVersion)
+			components.Versions[component] = newVersion
+		}
+	}
+
+	data, err := yaml.Marshal(&components)
+	if err != nil {
+		return fmt.Errorf("error while marshalling: %v", err)
+	}
+	data = []byte(versionFileComments + string(data))
+	err = os.WriteFile(fileP, data, 0o644)
+	if err != nil {
+		return fmt.Errorf("error writing file: %v", err)
+	}
+	return nil
+}
+
+func updateDepsVersionsFile(fileP string, components Components) error {
+	file, err := os.OpenFile(fileP, os.O_RDWR|os.O_TRUNC, 0o644)
+	if err != nil {
+		return fmt.Errorf("error opening file: %v", err)
+	}
+	table := tablewriter.NewWriter(file)
+	header := []string{OCPVersionHeader}
+	for component := range components.Repos {
+		header = append(header, component)
+	}
+	sort.Strings(header)
+	table.SetHeader(header)
+	table.SetAutoFormatHeaders(false)
+	table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+	table.SetCenterSeparator("|")
+
+	releaseVersion, err := semver.Make("4.10.0")
+	if err != nil {
+		return fmt.Errorf("error parsing the OCP version: %v", err)
+	}
+	var rows [][]string
+	for {
+		// We'll only/always consider components known in mainBranch.
+		versions := getVersions(releaseBranch(releaseVersion), components)
+		if len(versions) == 0 {
+			// TODO: increment major when needed.
+			break
+		}
+		versions[OCPVersionHeader] = releaseBranch(releaseVersion)
+		row := []string{}
+		for _, h := range header {
+			version := versionNotFound
+			if v, found := versions[h]; found {
+				version = v
+			}
+			row = append(row, version)
+		}
+		rows = append(rows, row)
+		err = releaseVersion.IncrementMinor()
+		if err != nil{
+			return err
+		}
+	}
+	// Latest version at the beginning.
+	slices.Reverse(rows)
+	table.AppendBulk(rows)
+	table.Render()
+	return nil
+}
+
+func versionWithLink(version, repo, ref string) string {
+	return fmt.Sprintf("[%s](%s)", version, fmt.Sprintf("https://github.com/%s/blob/%s", repo, ref))
+}
+
+func releaseBranch(version semver.Version) string {
+	return fmt.Sprintf("release-%d.%d", version.Major, version.Minor)
+}
+
+func getVersions(ref string, components Components) map[string]string {
+	versions := map[string]string{}
+	for component, repo := range components.Repos {
+		rawVersion, err := getVersion(repo, ref)
+		if err != nil {
+			continue
+		}
+		versions[component] = versionWithLink(trimmedVersion(rawVersion), repo, ref)
+	}
+	return versions
+}
+
+func getVersion(repo, ref string) (string, error) {
+	baseURL := fmt.Sprintf("https://raw.githubusercontent.com/%s/%s", repo, ref)
+	link := fmt.Sprintf("%s/VERSION", baseURL)
+	if repo == metricsServerRepo {
+		link = fmt.Sprintf("%s/manifests/release/kustomization.yaml", baseURL)
+	}
+	raw, err := fetchVersion(link)
+	if err != nil {
+		return "", err
+	}
+	if repo == metricsServerRepo {
+		var data map[string]interface{}
+		err := yaml.Unmarshal([]byte(raw), &data)
+		if err != nil {
+			return "", err
+		}
+		// keep it simple: panic if it's no more the case.
+		raw = data["images"].([]interface{})[0].(map[string]interface{})["newTag"].(string)
+	}
+	return raw, nil
+}
+
+func fetchVersion(path string) (string, error) {
+	resp, err := retryablehttp.Get(path)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("non 200 response")
+	}
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
---
+++ hack/go/go.mod
@@ -0,0 +1,23 @@
+module github.com/openshift/cluster-monitoring-operator/hack/go
+
+go 1.21
+
+require (
+	github.com/blang/semver/v4 v4.0.0
+	github.com/hashicorp/go-retryablehttp v0.7.5
+	github.com/olekukonko/tablewriter v0.0.5
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hclog v1.5.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
