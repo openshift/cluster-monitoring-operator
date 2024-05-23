3b7bff118b0c4e382af1eefc09c59630cd9f05bc
Mario Fernandez <mariofer@redhat.com>
2024-05-23 16:25:22 +0200

    add pkg/generated from codegen
    Signed-off-by: Mario Fernandez <mariofer@redhat.com>

---
+++ pkg/apis/cmo/v1/groupversion_info.go
@@ -0,0 +1,29 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package v1 contains API Schema definitions for the example v1 API group
+// +kubebuilder:object:generate=true
+// +groupName=cmo.example.com
+package v1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+var (
+	// GroupVersion is group version used to register these objects
+	GroupVersion = schema.GroupVersion{Group: "cmo.example.com", Version: "v1"}
+)
