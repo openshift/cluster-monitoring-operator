b1dd16df798bbecfcbfe05b93f5985add32f161e
Ayoub Mrini <amrini@redhat.com>
2025-08-04 09:03:35 +0200

    tweak

--- test/monitoring/monitoring_suite_test.go
+++ test/monitoring/monitoring_suite_test.go
@@ -12,7 +12,7 @@ func TestMonitoring(t *testing.T) {
 	RegisterFailHandler(Fail)
 	suiteConfig, reporterConfig := GinkgoConfiguration()
 	// Hardcoded until we find an easy way to pass it via "go test"
-	suiteConfig.Timeout = 3 * time.Hour
+	suiteConfig.Timeout = 4 * time.Hour
 	reporterConfig.NoColor = true
 	RunSpecs(t, "Monitoring Suite", suiteConfig, reporterConfig)
 }
