910c61b8f43345ce39599ff1b9f959962d1e96c3
Junqi Zhao <juzhao@redhat.com>
2025-09-28 22:01:49 +0800

    fix error

--- test/e2e/framework/assertions.go
+++ test/e2e/framework/assertions.go
@@ -11,7 +11,6 @@ import (
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
-	networkingv1 "k8s.io/api/networking/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
